56cdd5e16d2c40239e4244f43ce87df6da9b9f4f
Cat Norris <cannenorris@yahoo.com>
2018-05-06 21:55:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,7 @@ attr_accessor :name, :artist, :genre
   end
 
   def self.artist_count
-    artist_count= {}
+    artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
         artist_count [artist] += 1
